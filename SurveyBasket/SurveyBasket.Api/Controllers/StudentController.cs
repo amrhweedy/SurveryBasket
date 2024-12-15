7e5da73b549afc1fcd40428a4b03206df311a64a
amrhweedy <amrhweedy@gmail.com>
2024-12-15 23:36:51 +0200

    create student and studentResponse model and make some configuration form mapping between them (important), create student contoller

---
+++ SurveyBasket/SurveyBasket.Api/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿namespace SurveyBasket.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class StudentController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+
+        var student = new Student()
+        {
+            Id = 1,
+            FirstName = "amr",
+            MiddleName = "ahmed",
+            LastName = "hweedy",
+            DateOfBirth = new DateTime(1997, 8, 27),
+            Department = new Department
+            {
+                Id = 1,
+                Name = "IT"
+            }
+
+        };
+
+        var studentResponse = student.Adapt<StudentResponse>();
+        return Ok(studentResponse);
+    }
+
+}
--- SurveyBasket/SurveyBasket.Api/Mapping/MappingConfigurations.cs
+++ SurveyBasket/SurveyBasket.Api/Mapping/MappingConfigurations.cs
@@ -4,7 +4,15 @@ public class MappingConfigurations : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-        config.NewConfig<Poll, PollResponse>()
-                  .Map(dest => dest.Notes, src => src.Description);
+        config.NewConfig<Student, StudentResponse>()
+            .Map(dest => dest.FullName, src => $"{src.FirstName} {src.MiddleName} {src.LastName}")
+            .Map(dest => dest.Age, src => DateTime.Now.Year - src.DateOfBirth!.Value.Year,
+                  srcCondition => srcCondition.DateOfBirth.HasValue)
+           // .Map(dest => dest.DepartmentName, src => src.Department.Name)   this mapping will happen automatically because the name convention 
+           .Ignore(dest => dest.DepartmentName);  // DepartementName will be ignored from mapping in the response 
+
+        // mapping from student to studentResponse and vice versa
+        // config.NewConfig<Student, StudentResponse>().TwoWays();
+
     }
 }
