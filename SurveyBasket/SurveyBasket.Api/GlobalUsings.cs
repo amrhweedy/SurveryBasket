c699d63d62cb6b91ab5469093c14d2ebc4da6c1e
amrhweedy <amrhweedy@gmail.com>
2024-12-17 00:49:27 +0200

    .

--- SurveyBasket/SurveyBasket.Api/GlobalUsings.cs
+++ SurveyBasket/SurveyBasket.Api/GlobalUsings.cs
@@ -1,7 +1,15 @@
-﻿global using Mapster;
+﻿global using FluentValidation;
+global using FluentValidation.AspNetCore;
+global using Mapster;
+global using MapsterMapper;
 global using Microsoft.AspNetCore.Mvc;
+global using SurveyBasket.Api;
 global using SurveyBasket.Api.Contracts.Requests;
 global using SurveyBasket.Api.Contracts.Responses;
 global using SurveyBasket.Api.Models;
 global using SurveyBasket.Api.Services;
+global using System.ComponentModel.DataAnnotations;
+global using System.Reflection;
+
+
 
